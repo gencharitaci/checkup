d236a10eaa371420c4d44caa29de80135ab7aad4
Adem Kurtipek <gencharitaci@gmail.com>
2025-01-29 13:48:47 -0500

    Updated node packages

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,44 +1,54 @@
 <script>
-export let title
-export let description
-export let api = null
-export let img = null
+  export let title;
+  export let description;
+  export let api = null;
+  export let img = null;
 
-let json
-let err = false
+  /**
+   * @type {any}
+   */
+  let json;
+  let err = false;
 
-if (api) {
-  fetch(api)
-    .then(response => response.json())
-    .then(data => json = data)
-    .catch(e => err = true)
-}
+  if (api) {
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => (json = data))
+      .catch((e) => (err = true));
+  }
 
-function setError() {
-  err = true
-}
-
+  function setError() {
+    err = true;
+  }
 </script>
 
-
 <div class="w-full sm:w-1/1 md:w-1/2 lg:w-1/3 m-4">
-  <div class="max-w-sm rounded overflow-hidden shadow-lg {err ? 'border-8 border-red-600' : ''}">
+  <div
+    class="max-w-sm rounded overflow-hidden shadow-lg {err
+      ? 'border-8 border-red-600'
+      : ''}"
+  >
     <!-- <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"> -->
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">{title}</div>
-        <p class="text-gray-700 text-base">
-          {description}
-        </p>
-      </div>
-      <div class="px-6 py-4">
-        {#if img}
-        <img src="{img}" alt="service test" class="block m-auto" onerror={setError}>
-        {/if}
-        {#if json}
+      <p class="text-gray-700 text-base">
+        {description}
+      </p>
+    </div>
+    <div class="px-6 py-4">
+      {#if img}
+        <img
+          src={img}
+          alt="service test"
+          class="block m-auto"
+          on:error={setError}
+        />
+      {/if}
+      {#if json}
         <code class="text-green-700">
-          {JSON.stringify(json, null, '  ')}
+          {JSON.stringify(json, null, "  ")}
         </code>
-        {/if}
-      </div>
+      {/if}
     </div>
   </div>
+</div>
--- src/lib/ETLLog.svelte
+++ src/lib/ETLLog.svelte
@@ -1,52 +1,24 @@
 <script>
-let rows = []
+  /**
+   * @type {any[]}
+   */
+  let rows = [];
 
-fetch('https://mcmap.org/api2/v1/query/etl_status?columns=*&sort=table_name')
-  .then(response => response.json())
-  .then(json => {
-    rows = json
-    console.log(rows)
-  })
+  fetch("https://mcmap.org/api2/v1/query/etl_status?columns=*&sort=table_name")
+    .then((response) => response.json())
+    .then((json) => {
+      rows = json;
+      console.log(rows);
+    });
 
-function highlight(status) {
-  console.log(status);
-  return status !== 'success' ? 'highlight' : ''
-}
+  /**
+   * @param {string} status
+   */
+  function highlight(status) {
+    console.log(status);
+    return status !== "success" ? "highlight" : "";
+  }
 </script>
-
-<style>
-
-
-.highlight {
-  @apply bg-red-700 text-white;
-}
-
-@media screen and (max-width: 639px) {
-  .table-component thead { display: none; }
-
-  .table-component tr {
-    display: block;
-    margin: 0.75rem 0;
-    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-    border-radius: .25rem;
-    /* @apply block my-3 shadow rounded; */
-  }
-
-  .table-component td {
-    display: block;
-    text-align: right;
-    /* @apply block text-right; */
-  }
-
-  .table-component td:before {
-    content: attr(data-label);
-    text-transform: uppercase;
-    float: left;
-    font-weight: bold;
-    /* @apply uppercase float-left font-bold; */
-  }
-}
-</style>
 
 <table class="table-component table-auto w-full mt-8">
   <thead>
@@ -54,16 +26,58 @@ function highlight(status) {
       <th class="uppercase text-left px-4 py-2 text-gray-600">Script</th>
       <th class="uppercase text-left px-4 py-2 text-gray-600">Date</th>
       <th class="uppercase text-left px-4 py-2 text-gray-600 text">Status</th>
-
     </tr>
   </thead>
   <tbody>
     {#each rows as row, idx}
-      <tr class="{row.status.toUpperCase().indexOf('ERROR') !== -1 || row.status === 'failure' ? 'highlight' : ''}">
+      <tr
+        class={row.status.toUpperCase().indexOf("ERROR") !== -1 ||
+        row.status === "failure"
+          ? "highlight"
+          : ""}
+      >
         <td data-label="script" class="px-4 py-2">{row.table_name}</td>
-        <td data-label="date" class="px-4 py-2">{new Date(row.update_time).toLocaleString()}</td>
+        <td data-label="date" class="px-4 py-2"
+          >{new Date(row.update_time).toLocaleString()}</td
+        >
         <td data-label="status" class="px-4 py-2">{row.status}</td>
       </tr>
     {/each}
   </tbody>
 </table>
+
+<style>
+  .highlight {
+    @apply bg-red-700 text-white;
+  }
+
+  @media screen and (max-width: 639px) {
+    .table-component thead {
+      display: none;
+    }
+
+    .table-component tr {
+      display: block;
+      margin: 0.75rem 0;
+      box-shadow:
+        0 1px 3px 0 rgba(0, 0, 0, 0.1),
+        0 1px 2px 0 rgba(0, 0, 0, 0.06);
+      border-radius: 0.25rem;
+      /* @apply block my-3 shadow rounded; */
+    }
+
+    .table-component td {
+      display: block;
+      text-align: right;
+      /* @apply block text-right; */
+    }
+
+    .table-component td:before {
+      content: attr(data-label);
+      text-transform: uppercase;
+      float: left;
+      font-weight: bold;
+      /* @apply uppercase float-left font-bold; */
+    }
+  }
+</style>
